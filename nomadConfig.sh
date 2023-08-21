7cb2c26610f12b66e9cb871502ea0d880a5a01e3
Jared Stemper <31346981+JaredStemper@users.noreply.github.com>
2023-08-21 01:16:09 -0500

    Update nomadConfig.sh

--- nomadConfig.sh
+++ nomadConfig.sh
@@ -4,8 +4,6 @@ sudo chmod -R 777 /RSM
 wget https://raw.githubusercontent.com/JaredStemper/Scripts/main/nested.tmux.conf -O /RSM/.tmux.conf
 sudo gem install tmuxinator
 echo "alias mux=tmuxinator">>~/.zshrc && echo "setopt append_history; setopt hist_ignore_dups" >> ~/.zshrc && source ~/.zshrc
-mkdir -p ~/.config/tmuxinator
-mv /RSM/tmuxinator/*.yml ~/.config/tmuxinator/
 
 cd /RSM
 
