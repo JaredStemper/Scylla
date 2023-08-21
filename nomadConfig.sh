cc8014de975b98557d81bd3dae68019715f4c22c
Jared Stemper <31346981+JaredStemper@users.noreply.github.com>
2023-08-21 01:29:21 -0500

    Update nomadConfig.sh

--- nomadConfig.sh
+++ nomadConfig.sh
@@ -1,9 +1,19 @@
-#to be ran after `nomad-mound`
+#to be ran after `nomad-mount` inside of /RSM
 
+#fix permissions error
 sudo chmod -R 777 /RSM
-wget https://raw.githubusercontent.com/JaredStemper/Scripts/main/nested.tmux.conf -O /RSM/.tmux.conf
+
+#prepare tmuxinator for usage
 sudo gem install tmuxinator
 echo "alias mux=tmuxinator">>~/.zshrc && echo "setopt append_history; setopt hist_ignore_dups" >> ~/.zshrc && source ~/.zshrc
+
+#grab tmuxinator files
+git clone https://github.com/JaredStemper/mux.git
+mv mux/tmux.conf /RSM/.tmux.conf
+mkdir -p ~/.config/tmuxinator
+mv -t ~/.config/tmuxinator mux/*.yml
+mv mux/prefillTest.py /RSM/prefillTest.py
+mv mux/tmuxSessionHistoryCapture.sh /RSM/tmuxSessionHistoryCapture.sh
 
 cd /RSM
 
