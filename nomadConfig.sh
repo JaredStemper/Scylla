7ac76b2784da04ccdce01c8db8fbd28706f112db
Jared Stemper <31346981+JaredStemper@users.noreply.github.com>
2023-09-19 16:25:01 -0500

    adjusted relative references
    if tester was not in expected folder then it would cause issues before this

--- nomadConfig.sh
+++ nomadConfig.sh
@@ -9,10 +9,11 @@ sudo gem install tmuxinator
 echo "alias mux=tmuxinator">>~/.zshrc && echo "setopt append_history; setopt hist_ignore_dups" >> ~/.zshrc && source ~/.zshrc
 
 #grab tmuxinator files
+cd /RSM
 git clone https://github.com/JaredStemper/mux.git
-cp mux/tmux.conf ~/.tmux.conf
-mv mux/prefillTest.py /RSM/prefillTest.py
-mv mux/tmuxSessionHistoryCapture.sh /RSM/tmuxSessionHistoryCapture.sh
+cp /RSM/mux/tmux.conf ~/.tmux.conf
+mv /RSM/mux/prefillTest.py /RSM/prefillTest.py
+mv /RSM/mux/tmuxSessionHistoryCapture.sh /RSM/tmuxSessionHistoryCapture.sh
 
 #enable usage of TIOCSTI for prefill tool to work (more details https://bugs.archlinux.org/task/77745 and https://lore.kernel.org/linux-hardening/20221015041626.1467372-2-keescook@chromium.org/
 sudo sysctl -w dev.tty.legacy_tiocsti=1
