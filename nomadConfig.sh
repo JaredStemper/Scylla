2e055d7411d1a121351bbe3017be012804e2c2f5
Jared Stemper <jaredastemper+muxGithub@gmail.com>
2024-01-05 12:29:45 -0600

    moved prefill/mux logging files back into git dir
    this will keep the initial directory cleaner and remove some moving parts

--- nomadConfig.sh
+++ nomadConfig.sh
@@ -12,15 +12,13 @@ echo "alias mux=tmuxinator">>~/.bash_aliases && echo "setopt append_history; set
 cd /RSM
 git clone https://github.com/JaredStemper/mux.git
 cp /RSM/mux/tmux.conf ~/.tmux.conf
-mv /RSM/mux/prefillTest.py /RSM/prefillTest.py
-mv /RSM/mux/tmuxSessionHistoryCapture.sh /RSM/tmuxSessionHistoryCapture.sh
 
 #enable usage of TIOCSTI for prefill tool to work (more details https://bugs.archlinux.org/task/77745 and https://lore.kernel.org/linux-hardening/20221015041626.1467372-2-keescook@chromium.org/
 sudo sysctl -w dev.tty.legacy_tiocsti=1
 
 #create crontab to log all data captured in tmux currently on nomad every 15 minutes
-(crontab -l ; echo "0,15,30,45 * * * * /bin/bash /RSM/tmuxSessionHistoryCapture.sh") | crontab -
+(crontab -l ; echo "0,15,30,45 * * * * /bin/bash /RSM/mux/tmuxSessionHistoryCapture.sh") | crontab -
 
 cd /RSM; clear
 
-python3 /RSM/prefillTest.py "mux start -p /RSM/mux/tmuxinator/internalTemplate-initScan.yml msfWorkspace=CLIENTNAME domain=domain.local nessusKey=NESSUSKEY nomadPass='nomadPass'"
+python3 /RSM/mux/prefillTest.py "mux start -p /RSM/mux/tmuxinator/internalTemplate-initScan.yml msfWorkspace=CLIENTNAME domain=domain.local nessusKey=NESSUSKEY nomadPass='nomadPass'"
