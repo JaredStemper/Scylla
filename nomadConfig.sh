040f048c2011da182b8866148d91938eb066cedb
Jared Stemper <JaredStemper@users.noreply.github.com>
2024-06-10 12:36:43 -0500

    fix non-sourced alias in current shell

--- nomadConfig.sh
+++ nomadConfig.sh
@@ -23,15 +23,15 @@ echo "alias mux=tmuxinator; alias j='cd ..'">>~/.zshrc && echo "setopt append_hi
 echo "alias mux=tmuxinator; alias j='cd ..'">>~/.bash_aliases && echo "setopt append_history; setopt hist_ignore_dups" >> ~/.zshrc && source ~/.bash_aliases
 #grab tmuxinator files
 cd /RSM
-rm -rf /RSM/mux && git clone https://github.com/JaredStemper/mux.git
-cp /RSM/mux/tmux.conf ~/.tmux.conf
+rm -rf /RSM/mux && git clone https://github.com/JaredStemper/Scylla.git
+cp /RSM/Scylla/tmux.conf ~/.tmux.conf
 
 #enable usage of TIOCSTI for prefill tool to work (more details https://bugs.archlinux.org/task/77745 and https://lore.kernel.org/linux-hardening/20221015041626.1467372-2-keescook@chromium.org/
 sudo sysctl -w dev.tty.legacy_tiocsti=1
 
 #create crontab to log all data captured in tmux currently on nomad every 15 minutes
-(crontab -l ; echo "0,15,30,45 * * * * /bin/bash /RSM/mux/tmuxSessionHistoryCapture.sh") | crontab -
+(crontab -l ; echo "0,15,30,45 * * * * /bin/bash /RSM/Scylla/tmuxSessionHistoryCapture.sh") | crontab -
 
 cd /RSM; clear
 
-python3 /RSM/mux/prefillTest.py "mux start -p /RSM/mux/tmuxinator/internalTemplate-initScan.yml msfWorkspace=CLIENTNAME domain=domain.local nessusKey=NESSUSKEY nomadPass='nomadPass'"
+python3 /RSM/Scylla/prefillTest.py "tmuxinator start -p /RSM/Scylla/tmuxinator/internalTemplate-initScan.yml msfWorkspace=CLIENTNAME domain=domain.local nessusKey=NESSUSKEY nomadPass='nomadPass'"
